ce8f08a621c9448f63ad4e058a1f9749dac9d71e
Utsavladia <utsavladia@gmail.com>
2024-02-13 23:55:22 +0530

    message and send button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import ChatScreen from "./ChatScreen";
 
 function App() {
+  const [showChat, setShowChat] = useState(false);
+  const handleStartChat = () => {
+    setShowChat(!showChat);
+  };
   return (
     <div className="App">
-      <ChatScreen />
+      <h1 className="fixed top-0 left-1/2 -translate-x-1/2 text-xl font-bold text-green-700 text-center w-full">
+        Chat App
+      </h1>
+
+      <div className="w-full flex justify-evenly items-center py-10 min-h-lvh">
+        {!showChat ? (
+          <button
+            onClick={handleStartChat}
+            type="button"
+            class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-lg px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
+            Start Chatting
+          </button>
+        ) : (
+          <ChatScreen />
+        )}
+      </div>
     </div>
   );
 }
--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const ChatScreen = () => {
+  const [inputText, setInputText] = useState("");
+  const [messages, setMessages] = useState([]);
+  const textareaRef = useRef(null);
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSendMessage = () => {
+    console.log("message send");
+    setInputText("");
+  };
+  useEffect(() => {
+    adjustTextareaHeight();
+  }, [inputText]);
+
+  const adjustTextareaHeight = () => {
+    const textarea = textareaRef.current;
+    if (!textarea) return;
+
+    textarea.style.height = "auto";
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  };
+
   return (
-    <div className="font-bold flex justify-center items-center h-lvh w-1vw text-lg">
-      this is the chat screen
+    <div className="">
+      <div className="">messages</div>
+      <div className="fixed bottom-0 inset-x-0 h-auto flex items-center justify-center gap-4 py-4 px-4 bg-zinc-700">
+        <textarea
+          ref={textareaRef}
+          rows={1}
+          className="font-medium rounded-xl text-lg px-8 py-1.5 w-1/2 resize-none "
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="Type Something..."
+        />
+        <button
+          onClick={handleSendMessage}
+          type="button"
+          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-xl text-lg px-8 py-1.5 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        >
+          Send
+        </button>
+      </div>
     </div>
   );
 };
