a6b2a3e1bb48b27ad0aea8b430c8b3794115f122
Utsavladia <utsavladia@gmail.com>
2024-02-13 22:30:34 +0530

    initial commit

---
+++ src/ChatScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatScreen = () => {
+  return (
+    <div className="font-bold flex justify-center items-center h-lvh w-1vw text-lg">
+      this is the chat screen
+    </div>
+  );
+};
+
+export default ChatScreen;
